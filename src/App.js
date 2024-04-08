8b493c155f72591a63222d19fea2ab7df1f64165
Your New Username <singhdilpreet62790@gmail.com>
2024-04-08 16:32:05 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      This is my first program in which i am going to learn git and github ok
+      Autor : Dilpavittar pal singh
+      test: testing
+    </div>
+  );
+}
+
+export default App;
