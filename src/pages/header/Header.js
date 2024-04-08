bf2e26d86814d7a50a97e0774885487e4adbae3d
Your New Username <singhdilpreet62790@gmail.com>
2024-04-08 17:56:14 +0530

    add content

--- src/pages/header/Header.js
+++ src/pages/header/Header.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./header.css";
+import Content from "./Content";
 function Header() {
   return (
     <div>
       <h3>This is header content</h3>
+      <Content />
     </div>
   );
 }
